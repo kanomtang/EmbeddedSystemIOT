3452ff1ec047c502cf85fab55934c93af12507a8
kanomtang <rungroj_s@cmu.ac.th>
2017-11-30 23:54:50 +0700

    Create unitest for all method in python file

--- testEmbeddedSystem.py
+++ testEmbeddedSystem.py
@@ -1,9 +1,33 @@
+#import FinalFBnoCam
+import FinalProgressWithCam
 import unittest
-import EmbbedSystem
-
-
 class MyTestCase(unittest.TestCase):
+
+
     def testRetreiveData(self):
-        self.assertEqual(EmbbedSystem.RetreiveData(), True)
+        itemlist, itemlist_key, CustomerAddress = FinalFBnoCam.RetreiveData()
+        self.assertEqual(CustomerAddress,'Aumphur Muang,Chiangmai')
+        self.assertEqual(itemlist_key[0],'-KzyX5tD07R7qv87dEdS')
+        self.assertEqual(itemlist[0]['price'],30)
+    def testCreatArrayKey(self):
+        rawString = '"Test1,Test2"'
+
+        actualResult = FinalFBnoCam.CreateArrayOfKeyDict(rawString)
+
+        self.assertEqual(actualResult[0],'Test1')
+        self.assertEqual(actualResult[1],'Test2')
+
+    def testReplaceValues(self):
+        rawString = '/] Hello ['
+        actualResult = FinalFBnoCam.ReplaceValuesInDict(rawString)
+        rawString1 = '[]/ M[ e ]t h/ od'
+        actualResult1 = FinalFBnoCam.ReplaceValuesInDict(rawString1)
+        self.assertEqual(actualResult,'Hello')
+        self.assertEqual(actualResult1,'Method')
+
+    def testUpdate(self):
+        a = ['Hello','World','by','Python']
+        actualResult = FinalFBnoCam.UpdateNewValuesDictKey(a)
+        self.assertEqual(actualResult,'Hello,World,by,Python')
 if __name__ == '__main__':
     unittest.main()
