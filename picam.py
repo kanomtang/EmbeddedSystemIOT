1e175c2d464c9034c2734abc1158c5bf259ce7d8
CMU\rungroj_s <rungroj029@gmail.com>
2018-03-20 19:55:09 +0700

    another

--- picam.py
+++ picam.py
@@ -1,44 +1,33 @@
-import io
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 import time
-import picamera
-from PIL import Image
+import cv2
 import zbar
-
-
-	# Create the in-memory stream
-stream = io.BytesIO()
-with picamera.PiCamera() as camera:
-	camera.start_preview()
-	time.sleep(2)
-	camera.capture(stream, format='jpeg')
-	# "Rewind" the stream to the beginning so we can read its content
-stream.seek(0)
-pil = Image.open(stream)
-while True:
-
-	#
-	#########################################
-	#
-	# create a reader
-	scanner = zbar.ImageScanner()
-
-	# configure the reader
-	scanner.parse_config('enable')
-
-	pil = pil.convert('L')
-	width, height = pil.size
-	raw = pil.tostring()
-
-	# wrap image data
-	image = zbar.Image(width, height, 'Y800', raw)
-
-	# scan the image for barcodes
-	scanner.scan(image)
-
-	# extract results
-	for symbol in image:
-	    # do something useful with results
-	    print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
-
-	# clean up
-	del(image)
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+ 
+# allow the camera to warmup
+time.sleep(0.1)
+set_zbar = zbar.ImageScanner()
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image, then initialize the timestamp
+	# and occupied/unoccupied text
+	image = frame.array
+ 	set_zbar.scan(image)
+ 	for symbol in image:
+            print '\033[1;32mResult : %s symbol "%s" \033[1;m' % (symbol.type,symbol.data)
+	# show the frame
+	cv2.imshow("Frame", image)
+	key = cv2.waitKey(1) & 0xFF
+ 
+	# clear the stream in preparation for the next frame
+	rawCapture.truncate(0)
+ 
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
