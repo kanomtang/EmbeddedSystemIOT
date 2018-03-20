5a41427bdff5cd27a12fed567ab0f1cc8d09a539
CMU\rungroj_s <rungroj029@gmail.com>
2018-03-20 18:52:53 +0700

    picam

---
+++ picam.py
@@ -0,0 +1,42 @@
+import io
+import time
+import picamera
+from PIL import Image
+import zbar
+
+while True:
+	# Create the in-memory stream
+	stream = io.BytesIO()
+	with picamera.PiCamera() as camera:
+	    camera.start_preview()
+	    time.sleep(2)
+	    camera.capture(stream, format='jpeg')
+	# "Rewind" the stream to the beginning so we can read its content
+	stream.seek(0)
+	pil = Image.open(stream)
+	#
+	#########################################
+	#
+	# create a reader
+	scanner = zbar.ImageScanner()
+
+	# configure the reader
+	scanner.parse_config('enable')
+
+	pil = pil.convert('L')
+	width, height = pil.size
+	raw = pil.tostring()
+
+	# wrap image data
+	image = zbar.Image(width, height, 'Y800', raw)
+
+	# scan the image for barcodes
+	scanner.scan(image)
+
+	# extract results
+	for symbol in image:
+	    # do something useful with results
+	    print 'decoded', symbol.type, 'symbol', '"%s"' % symbol.data
+
+	# clean up
+	del(image)
