3bf452458da2638769f5496d8f7fa5465113b543
CMU\rungroj_s <rungroj029@gmail.com>
2018-03-20 19:59:08 +0700

    update

--- picam.py
+++ picam.py
@@ -3,7 +3,7 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
-import zbar
+ 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
@@ -12,15 +12,13 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
  
 # allow the camera to warmup
 time.sleep(0.1)
-set_zbar = zbar.ImageScanner()
+ 
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	# grab the raw NumPy array representing the image, then initialize the timestamp
 	# and occupied/unoccupied text
 	image = frame.array
- 	set_zbar.scan(image)
- 	for symbol in image:
-            print '\033[1;32mResult : %s symbol "%s" \033[1;m' % (symbol.type,symbol.data)
+ 
 	# show the frame
 	cv2.imshow("Frame", image)
 	key = cv2.waitKey(1) & 0xFF
